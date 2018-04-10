06c8c60f455ea270071b7a1708a46dea956d0c37
Victor Castell <victor@victorcastell.com>
2018-04-10 21:57:50 +0200

    Fmt'd

--- dkron/store.go
+++ dkron/store.go
@@ -133,7 +133,7 @@ func (s *Store) SetJobDependencyTree(job *Job, previousJob *Job) error {
 		defer pj.Unlock()
 
 		pj.DependentJobs = append(pj.DependentJobs, job.Name)
-		if err := s.SetJob(pj,nil); err != nil {
+		if err := s.SetJob(pj, nil); err != nil {
 			return err
 		}
 	}
@@ -155,7 +155,7 @@ func (s *Store) SetJobDependencyTree(job *Job, previousJob *Job) error {
 			}
 		}
 		pj.DependentJobs = append(pj.DependentJobs[:ndx], pj.DependentJobs[ndx+1:]...)
-		if err := s.SetJob(pj,nil); err != nil {
+		if err := s.SetJob(pj, nil); err != nil {
 			return err
 		}
 	}
@@ -170,7 +170,7 @@ func (s *Store) SetJobDependencyTree(job *Job, previousJob *Job) error {
 		defer pj.Unlock()
 
 		pj.DependentJobs = append(pj.DependentJobs, job.Name)
-		if err := s.SetJob(pj,nil); err != nil {
+		if err := s.SetJob(pj, nil); err != nil {
 			return err
 		}
 	}
