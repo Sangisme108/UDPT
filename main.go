48b17b759f748bebefc675f23a09da0f9b86e26a
Victor Castell <victor@victorcastell.com>
2018-08-01 15:30:58 +0200

    First stab at cobra

--- main.go
+++ main.go
@@ -2,56 +2,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/hashicorp/go-plugin"
-	"github.com/mitchellh/cli"
-	"github.com/victorcoder/dkron/dkron"
-)
-
-const (
-	VERSION = "0.9.8"
+	"github.com/victorcoder/dkron/cmd"
 )
 
 func main() {
-	c := cli.NewCLI("dkron", VERSION)
-	c.Args = os.Args[1:]
-	c.HelpFunc = cli.BasicHelpFunc("dkron")
-
-	ui := &cli.BasicUi{Writer: os.Stdout}
-
-	plugins := &Plugins{}
-	plugins.DiscoverPlugins()
-
-	// Make sure we clean up any managed plugins at the end of this
-
-	c.Commands = map[string]cli.CommandFactory{
-		"agent": func() (cli.Command, error) {
-			return &dkron.AgentCommand{
-				Ui:               ui,
-				Version:          VERSION,
-				ProcessorPlugins: plugins.Processors,
-			}, nil
-		},
-		"keygen": func() (cli.Command, error) {
-			return &dkron.KeygenCommand{
-				Ui: ui,
-			}, nil
-		},
-		"version": func() (cli.Command, error) {
-			return &dkron.VersionCommand{
-				Version: VERSION,
-				Ui:      ui,
-			}, nil
-		},
-	}
-
-	exitStatus, err := c.Run()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error executing CLI: %s\n", err.Error())
-	}
-
-	plugin.CleanupClients()
-	os.Exit(exitStatus)
+	cmd.Execute()
 }
